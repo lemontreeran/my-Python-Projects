4d959cc77a182898849dfd7adbe05562c3b12389
lemontreeran <ran.xing@gmail.com>
2019-07-29 11:22:55 +1000

    Update install-packer-linux.sh

--- install-packer-linux.sh
+++ install-packer-linux.sh
@@ -6,7 +6,7 @@ pversion=0.10.1
 which packer || {
   mkdir -p $workdir
   cd $workdir
-  apt-get -qqy install unzip wget
+  sudo yum install unzip wget
   wget https://releases.hashicorp.com/packer/$pversion/packer_${pversion}_linux_amd64.zip && \
   unzip packer_${pversion}_linux_amd64.zip && \
   mv packer /usr/local/bin/
