a702533a40999c5be3100e533f48f416282e2d86
lemontreeran <ran.xing@gmail.com>
2019-07-29 11:15:13 +1000

    Create install-packer-linux.sh

---
+++ install-packer-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+workdir=/tmp/packer
+pversion=0.10.1
+
+which packer || {
+  mkdir -p $workdir
+  cd $workdir
+  apt-get -qqy install unzip wget
+  wget https://releases.hashicorp.com/packer/$pversion/packer_${pversion}_linux_amd64.zip && \
+  unzip packer_${pversion}_linux_amd64.zip && \
+  mv packer /usr/local/bin/
+  rm -Rf $workdir
+  
+  packer -v
+}
+echo "Go ahead and enjoy packer"
